58626cf78f44a01ffad3406471cdc1d7aa039f2b
Juan Manuel Ramallo <vendeforex@gmail.com>
2013-11-30 01:31:05 -0200

    Update nav.php

--- nav.php
+++ nav.php
@@ -8,8 +8,8 @@ if(ereg($sitio,$url)) {echo 'id="activo"'; }
 <header>
 
 <nav>
-<div class=menue <?php  Menu('http://hacerpatito.com/menu/index.php')  ?>  ><a href="http://hacerpatito.com/menu/index.php" >Home</a></div>
-<div class=menue <?php  Menu('http://hacerpatito.com/menu/nosotros.php') 	  ?>  ><a href="http://hacerpatito.com/menu/nosotros.php">Nosotros</a>   </div>
-<div class=menue <?php Menu('http://hacerpatito.com/menu/contacto.php')        ?>  ><a href="http://hacerpatito.com/menu/contacto.php">Contacto</a>   </div</nav>
+<div class=menue <?php  Menu('http://ejemploaquituweb/index.php')  ?>  ><a href="http://ejemploaquituweb/index.php" >Home</a></div>
+<div class=menue <?php  Menu('http://ejemploaquituweb/nosotros.php')           ?>  ><a href="http://ejemploaquituweb/nosotros.php">Nosotros</a>   </div>
+<div class=menue <?php Menu('http://ejemploaquituweb/contacto.php')        ?>  ><a href="http://ejemploaquituweb/contacto.php">Contacto</a>   </div</nav>
 </nav>
 </header>
