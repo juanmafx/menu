2d09618c914b501d1220d7868a201d021b7cb711
Juan Manuel Ramallo <vendeforex@gmail.com>
2013-11-30 01:11:30 -0200

    Create contacto.php

---
+++ contacto.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<title>Contacto</title>
+<link href="css.css" rel="stylesheet">
+</head>
+<?php
+include 'nav.php';
+?>
+
+<article>
+<h1>Parte de contacto</h1>
+<p>
+En contacto seria muy bueno colocar los numeros de telefono, mails, como facebook y todo para tus uduarios.
+</p>
+
+</article>
+<body>
+</html>
