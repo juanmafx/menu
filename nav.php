c7398b247b08e04f61f3298a0b52e8b8f9698496
Juan Manuel Ramallo <vendeforex@gmail.com>
2013-11-30 02:06:47 -0200

    Listo y ordenado
    Ordenar un poco el menu php

--- nav.php
+++ nav.php
@@ -4,12 +4,13 @@ $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
 if(ereg($sitio,$url)) {echo 'id="activo"'; }
 }
  ?>
- 
+
 <header>
 
 <nav>
-<div class=menue <?php  Menu('http://ejemploaquituweb/index.php')  ?>  ><a href="http://ejemploaquituweb/index.php" >Home</a></div>
-<div class=menue <?php  Menu('http://ejemploaquituweb/nosotros.php')           ?>  ><a href="http://ejemploaquituweb/nosotros.php">Nosotros</a>   </div>
-<div class=menue <?php Menu('http://ejemploaquituweb/contacto.php')        ?>  ><a href="http://ejemploaquituweb/contacto.php">Contacto</a>   </div</nav>
+<div class=menue <?php  Menu('http://ejemploaquituweb/index.php')     ?>  ><a href="http://ejemploaquituweb/index.php" >Home</a></div>
+<div class=menue <?php  Menu('http://ejemploaquituweb/nosotros.php')  ?>  ><a href="http://ejemploaquituweb/nosotros.php">Nosotros</a>   </div>
+<div class=menue <?php Menu('http://ejemploaquituweb/contacto.php')   ?>  ><a href="http://ejemploaquituweb/contacto.php">Contacto</a>   </div</nav>
 </nav>
+
 </header>
