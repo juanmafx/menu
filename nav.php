3006a36ece48f90ba787eaae6dfbf13c53533a2c
Juan Manuel Ramallo <vendeforex@gmail.com>
2014-01-02 21:35:24 -0200

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,16 +1,14 @@
 <?
 function Menu($sitio) {
 $url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
-if(ereg($sitio,$url)) {echo 'class="activo"'; }
+if(ereg($sitio,$url)) {echo 'class="active"'; }
 }
  ?>
 
 <header>
-
 <nav>
 <div class=menue <?php  Menu('http://ejemploaquituweb/index.php')     ?>  ><a href="http://ejemploaquituweb/index.php" >Home</a></div>
 <div class=menue <?php  Menu('http://ejemploaquituweb/nosotros.php')  ?>  ><a href="http://ejemploaquituweb/nosotros.php">Nosotros</a>   </div>
 <div class=menue <?php Menu('http://ejemploaquituweb/contacto.php')   ?>  ><a href="http://ejemploaquituweb/contacto.php">Contacto</a>   </div</nav>
 </nav>
-
 </header>
