282d8d5ff758c1968532cb14068ad7e18e5d7402
Juan Manuel Ramallo <jramallo1989@gmail.com>
2018-01-11 14:20:12 -0300

    Update nav.php

--- nav.php
+++ nav.php
@@ -9,6 +9,6 @@ if(ereg($sitio,$url)) {echo 'class="active"'; }
 <nav>
 <div class=menue <?php  Menu('http://ejemploaquituweb/index.php')     ?>  ><a href="http://ejemploaquituweb/index.php" >Home</a></div>
 <div class=menue <?php  Menu('http://ejemploaquituweb/nosotros.php')  ?>  ><a href="http://ejemploaquituweb/nosotros.php">Nosotros</a>   </div>
-<div class=menue <?php Menu('http://ejemploaquituweb/contacto.php')   ?>  ><a href="http://ejemploaquituweb/contacto.php">Contacto</a>   </div</nav>
+<div class=menue <?php Menu('http://ejemploaquituweb/contacto.php')   ?>  ><a href="http://ejemploaquituweb/contacto.php">Contacto</a>   </div></nav>
 </nav>
 </header>
